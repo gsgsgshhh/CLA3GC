4caad75f0cf6531472d725ea1121343aa4589524
gsgsgshhh <90181275+gsgsgshhh@users.noreply.github.com>
2025-09-12 11:02:13 +0800

    Update CLA3GC.py

--- CLA3GC.py
+++ CLA3GC.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from data_loader import datasets
 from graph_filtering import multi_view_processing
@@ -6,29 +5,18 @@ from anchor import sampling_kmeans, sampling_minikmeans
 from clustering import Effecient_clustering, Effecient_multi_view_clustering, New_Effecient_clustering
 from metrics import evaluate_clustering
 import pandas as pd
-
-
-
 import time
 
-class eagsc():
+class clagc():
     def __init__(self, dataname="Citeseer"):
         self.datasets = datasets
         self.alphas = [0.001, 1, 10, 100, 1000, 10000]
         self.betas = [0.001, 1, 10, 100, 1000, 10000]
-        # self.gammas = [0.001, 0.1, 1, 10]
         self.data_name = dataname
         self.data_type = 'graph data'
         self.max_dims = 1300
-        # self.algorithms = [New_Effecient_clustering, Effecient_multi_view_clustering]
         self.algorithms = [Effecient_clustering, Effecient_multi_view_clustering]
         self.k = 2
-        # self.best_paras = {
-        #     "alpha": 0,
-        #     "beta": 0,
-        #     "gamma": 0,
-        #     "m": 0,
-        # }
         self.best_paras = {
             "alpha": 0,
             "beta": 0,
@@ -115,39 +103,6 @@ class eagsc():
                         self.best_paras["beta"] = beta
                         self.best_paras["m"] = m
 
-    # 带有双重锚优化的训练
-    # def train(self):
-    #     H = self.graphFiltering()
-    #     self.showData()
-    #     for m in self.ms:
-    #         if m < self.C:
-    #             continue
-    #         B = self.initailizeB(m=m, H=H)
-    #         for alpha in self.alphas:
-    #             for beta in self.betas:
-    #                 for gamma in self.gammas:
-    #                     if self.V == 1:
-    #                         Z, _ = self.algorithms[0](H[0], B[0], alpha=alpha, beta=beta, gamma=gamma)
-    #                     else:
-    #                         Z, _, _ = self.algorithms[1](H, B, alpha=alpha, beta=beta)
-    #
-    #                     acc, nmi, ari, f1, pur = evaluate_clustering(Z, self.gnd)
-    #                     print(
-    #                         "m: {0: <3} alpha: {1: <5} beta: {2: <5} gamma: {3: <5} ACC: {4:.4f} NMI: {5:.4f} ARI: {6:.4f} F1: {7:.4f} PUR: {8:.4f}".format(
-    #                             m, alpha, beta, gamma, acc, nmi, ari, f1, pur,
-    #                         )
-    #                     )
-    #                     if acc > self.best_re["ACC"]:
-    #                         self.best_re["ACC"] = acc
-    #                         self.best_re["NMI"] = nmi
-    #                         self.best_re["ARI"] = ari
-    #                         self.best_re["F1"] = f1
-    #                         self.best_re["PUR"] = pur
-    #                         self.best_paras["alpha"] = alpha
-    #                         self.best_paras["beta"] = beta
-    #                         self.best_paras["gamma"] = gamma
-    #                         self.best_paras["m"] = m
-
     def reproduce(self):
         H = self.graphFiltering()
         B = self.initailizeB(m=self.best_paras["m"], H=H)
@@ -167,32 +122,12 @@ class eagsc():
                     )
             )
 
-    # 带有双重锚优化
-    # def reproduce(self):
-    #     H = self.graphFiltering()
-    #     B = self.initailizeB(m=self.best_paras["m"], H=H)
-    #     time_begin = time.time()
-    #     if self.V == 1 :
-    #         Z, _ = self.algorithms[0](H[0].copy(), B[0].copy(), alpha=self.best_paras["alpha"], beta=self.best_paras["beta"], gamma=self.best_paras["gamma"])
-    #     else :
-    #         Z, _ = self.algorithms[1](H.copy(), B.copy(), alpha=self.best_paras["alpha"], beta=self.best_paras["beta"])
-    #
-    #     acc, nmi, ari, f1, pur = evaluate_clustering(Z, self.gnd)
-    #     time_end = time.time()
-    #     Time = np.fabs(time_end - time_begin)
-    #     self.best_re["Time"] = Time
-    #     print(
-    #         "k: {0: <3} m: {1: <3} alpha: {2: <5} beta: {3: <5} gamma: {4: <5} ACC: {5:.4f} NMI: {6:.4f} ARI: {7:.4f} F1: {8:.4f} PUR: {9:.4f} Time: {10:.2f}".format(
-    #                 self.k, self.best_paras["m"], self.best_paras["alpha"], self.best_paras["beta"], self.best_paras["gamma"], self.best_re["ACC"], nmi, ari, f1, pur, Time
-    #                 )
-    #         )
-
-
 if __name__ == "__main__":
     dataname = "Cora"
-    res = eagsc(dataname=dataname)
+    res = clagc(dataname=dataname)
     res.train()
     res.reproduce()
+
 
 
 
