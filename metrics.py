b0ff31739d224fe3ed2919ca456063a7385a38d9
gsgsgshhh <90181275+gsgsgshhh@users.noreply.github.com>
2025-09-12 11:08:32 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -3,12 +3,8 @@ import scipy.sparse as sp
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from Metrics import metric_all
 
-
 def evaluate_clustering(S, gnd):
     num_labels = len(np.unique(gnd))
-
-    # ! Y choose 30? Because the 31 is close to sqrt(1000). Th complexity of SVD is O(d^2N),I hope it's less than O(1000N)
-    # ! If number of labels is large, it's bearable that Tp=num_labels.
     Tp = max(num_labels, 30)
 
     if S.shape[0] <= Tp:
@@ -20,17 +16,14 @@ def evaluate_clustering(S, gnd):
             S, k=Tp, which='LM'
         )
 
-    # ! Perform MiniBatchKMeans to reduce the space cost while keeping the accuracy.
     if S.shape[1] > 20000:
         kmeans = MiniBatchKMeans(init="k-means++", batch_size=1000, n_clusters=num_labels, random_state=20).fit(v.T)
     elif S.shape[1] > 1e6:
         kmeans = MiniBatchKMeans(init="k-means++", batch_size=500, n_clusters=num_labels, random_state=20).fit(v.T)
     else:
-        # ! Perform KMeans on smaller graph.
         kmeans = KMeans(init="k-means++", n_clusters=num_labels, random_state=20).fit(v.T)
 
     predict_labels = kmeans.predict(v.T)
-
     re = metric_all.clustering_metrics(predict_labels, gnd)
     acc, nmi, ari, f1, pur = re.evaluationClusterModelFromLabel()
 
